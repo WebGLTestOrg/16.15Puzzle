93ec05baf6ab26a0139cd2f64b4a2456dabb3961
Alex <92122843+Taduar2001@users.noreply.github.com>
2025-08-06 14:45:14 +0300

    New

--- __settings__.js
+++ __settings__.js
@@ -1,15 +1,15 @@
-window.ASSET_PREFIX = "./build/";
+window.ASSET_PREFIX = "";
 window.SCRIPT_PREFIX = "";
 window.SCENE_PATH = "2290825.json";
 window.CONTEXT_OPTIONS = {
     'antialias': true,
-    'alpha': false,
+    'alpha': true,
     'preserveDrawingBuffer': false,
     'deviceTypes': [`webgl2`, `webgl1`],
     'powerPreference': "high-performance"
 };
 window.SCRIPTS = [ 243556683, 243556684, 243556685, 243556686, 243556687, 243556688, 243558270, 243558272, 243786356, 243797633, 243803178 ];
-window.CONFIG_FILENAME = window.ASSET_PREFIX +"config.json";
+window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
     useMouse: true,
